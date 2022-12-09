0842932b7650814f25b7c6fa6a300440e7339d7a
Navaneeth Rao <decodestudios.help@gmail.com>
2022-12-09 09:51:43 +0530

    animated autoscroll

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,11 +9,18 @@ module.exports = {
       fontFamily: { sans: "'Poppins', sans-serif" },
       animation: {
         "bounce-slow": "bounce-slow 2.5s infinite",
+        scroll: "scroll 10s linear infinite",
       },
       keyframes: {
         "bounce-slow": {
           "0%, 100%": { transform: "translateY(5%)" },
           "50%": { transform: "translateY(0)" },
+        },
+        scroll: {
+          "0%": { transform: "translateX(0)" },
+          "100%": {
+            transform: "translateX( calc( (-25% * 2.5) ) )",
+          },
         },
       },
     },
