60e2ae4284e46309634b091bd253a238a95a87e5
Navaneeth Rao <navaneeth@inflorencers.com>
2023-07-03 10:44:29 +0530

    added tags to schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,6 +8,7 @@ const blog = defineCollection({
     description: z.string(),
     pubDate: z.string().transform((str) => new Date(str)),
     image: z.string().optional().default("/sveltekit.png"),
+    tags: z.array(z.string()),
     isDraft: z.boolean().optional().default(false),
   }),
 });
