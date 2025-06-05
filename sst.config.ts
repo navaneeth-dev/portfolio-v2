526347bb0526525f011ab10847cf152900959e9b
Navaneeth <32010211+navaneeth-dev@users.noreply.github.com>
2025-06-05 09:02:14 +0530

    fix: dmarc

--- sst.config.ts
+++ sst.config.ts
@@ -18,6 +18,7 @@ export default $config({
     const sender = "rizexor.com";
     const email = $app.stage === "production" ? sst.aws.Email.get("MyEmail", sender) :
       new sst.aws.Email("MyEmail", {
+        dmarc: "v=DMARC1; p=quarantine; adkim=s",
         sender,
         dns: sst.cloudflare.dns()
       });
