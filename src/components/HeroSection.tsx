6c986126cd94efadc48ac64a2cac5c2feade5dd6
Navaneeth <32010211+navaneeth-dev@users.noreply.github.com>
2025-08-02 15:48:47 +0530

    feat: framer-motion

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,38 @@
+export default function HeroSection() {
+  return (
+    <main className="relative overflow-hidden">
+      <div
+        className="container px-12 mx-auto flex flex-col-reverse md:flex-row justify-around items-center py-6 max-w-screen-xl"
+      >
+        <div className="flex flex-1 flex-col gap-y-6 py-12">
+          <h1 className="text-center md:text-left text-2xl md:text-3xl">
+            Hi, my name is<br /><span className="font-bold">Navaneeth</span>
+          </h1>
+          <p className="text-center md:text-left">
+            I am a final year Computer Science undergrad student with more than 5 years of experience across Full Stack
+            development, Cyber Security and DevOps.
+          </p>
+          <a
+            href="/contact"
+            className="bg-primary text-white px-4 py-2 border border-black self-center md:self-start rounded hover:shadow-[5px_5px_0_rgba(0,0,0,1)] transition-shadow"
+          >Connect with me</a
+          >
+        </div>
+        <div className="flex-1 flex justify-center">
+          <picture className="z-10">
+            <source srcSet="/lock.webp" type="image/webp" />
+            <img src="/lock.png" alt="3D Lock" className="max-h-96 w-[500px] animate-bounce-slow" />
+          </picture>
+        </div>
+      </div>
+      <div
+        className="hidden md:block overflow-hidden bg-primary w-[55vw] h-[55vw] absolute -right-2 top-1/2 rotate-45 -translate-y-1/2 translate-x-1/2"
+      >
+      </div>
+      <div
+        className="bg-primary md:hidden absolute top-12 right-1/2 translate-x-1/2 translate-y-1/4 max-w-[9rem] max-h-[9rem] w-[38vw] h-[38vw] rotate-45"
+      >
+      </div>
+    </main>
+  );
+}
